a635fee0b852bbe309912c69dc9cec408aa53814
Chromgamer <stl20@web.de>
2025-06-22 18:36:50 +0200

    stat change

--- common/defines/hmm_defines.lua
+++ common/defines/hmm_defines.lua
@@ -1,4 +1,5 @@
 NDefines.NMilitary.UNIT_EXP_LEVELS = { 0.1, 0.3, 0.98, 0.99 }
+NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.125
 NDefines.NCountry.GIE_DIVISION_ATTACK_BONUS_AGAINST_OCCUPIER = 0 -- Attack bonus factor against whoever occupies your core territory.
 NDefines.NCountry.GIE_DIVISION_DEFENSE_BONUS_AGAINST_OCCUPIER = 0 -- Attack bonus factor against whoever occupies your core territory.
 NDefines.NCountry.GIE_DIVISION_ATTACK_BONUS_ON_CORE = 0 -- Attack bonus factor when fighting on cores.
